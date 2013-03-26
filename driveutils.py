881281c4d9dde55555722046ec233d9567eb9bbe
Fernando Arbeiza <farbeiza@bevalley.com>
2013-03-26 20:22:09 +0100

    Add driveutils.

---
+++ driveutils.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import re
+
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+
+FIELDS = 'id, title, mimeType, modifiedDate, md5Checksum, fileSize'
+
+MIME_FOLDER = 'application/vnd.google-apps.folder'
+
+SEARCH_PARAMETER_RE = re.compile('(' + '[' + '\'\\\\' + ']' + ')')
+SEARCH_PARAMETER_REPLACEMENT = '\\\\\\1'
+
+CLIENT_ID = '387402765904.apps.googleusercontent.com'
+CLIENT_SECRET = 'WTj0xKbLAFjDqUeT2HGDZHCi'
+
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+
+def escapeQueryParameter(parameter):
+    return SEARCH_PARAMETER_RE.sub(SEARCH_PARAMETER_REPLACEMENT, parameter)
+
+flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+
+url = flow.step1_get_authorize_url()
+print 'Please open the following URL: '
+print url
+authorizationCode = raw_input('Copy and paste the authorization code: ').strip()
+
+credentials = flow.step2_exchange(authorizationCode)
+
+http = httplib2.Http()
+http = credentials.authorize(http)
+
+DRIVE_SERVICE = build('drive', 'v2', http = http)
---
+++ test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import driveutils
+
+import httplib2
+import pprint
+
+from apiclient.http import MediaFileUpload
+
+# Path to the file to upload
+FILENAME = '/home/farbeiza/Escritorio/surgeries.txt'
+
+# Insert a file
+media_body = MediaFileUpload(FILENAME, mimetype='text/plain', resumable=True)
+body = {
+  'title': 'My document',
+  'description': 'A test document',
+  'mimeType': 'text/plain'
+}
+
+file = driveutils.DRIVE_SERVICE.files().insert(body=body, media_body=media_body).execute()
+pprint.pprint(file)
+
