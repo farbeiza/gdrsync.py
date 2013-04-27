063503879961d4b7e377e05932e790c9ab49f499
Fernando Arbeiza <farbeiza@bevalley.com>
2013-04-27 02:16:57 +0200

    Handle Google Drive special values.

--- date.py
+++ date.py
@@ -7,11 +7,15 @@ import functools
 MS = 1000 # milliseconds / second
 US = MS * MS # microseconds / second
 
-DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
-
 # Time resolution in Google Drive is milliseconds
 SCALE = 3
 PRECISION = 10 ** SCALE
+
+DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+# INVALID_SECONDS is a special value used in Google Drive for empty Dates
+INVALID_SECONDS = 0.0
+SUBSTITUTE_SECONDS = float(1) / PRECISION
 
 def fromSeconds(seconds):
     return Date(seconds)
@@ -20,6 +24,8 @@ def fromString(string):
     dateTime = datetime.datetime.strptime(string, DATE_TIME_FORMAT)
     seconds = (calendar.timegm(dateTime.timetuple())
             + (float(dateTime.microsecond) / US))
+    if seconds == SUBSTITUTE_SECONDS:
+        seconds = INVALID_SECONDS
 
     return fromSeconds(seconds)
 
@@ -38,6 +44,10 @@ class Date(object):
         return round(self._seconds * PRECISION)
 
     def __str__(self):
-        dateTime = datetime.datetime.utcfromtimestamp(self._seconds)
+        seconds = self._seconds
+        if seconds == INVALID_SECONDS:
+            seconds = SUBSTITUTE_SECONDS
+
+        dateTime = datetime.datetime.utcfromtimestamp(seconds)
 
         return dateTime.strftime(DATE_TIME_FORMAT)
