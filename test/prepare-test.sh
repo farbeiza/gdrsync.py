eb0bc047dcbabb6d520d31203f87488572c8640f
Fernando Arbeiza <farbeiza@bevalley.com>
2013-04-27 02:25:44 +0200

    Add test for invalid date.

--- test/prepare-test.sh
+++ test/prepare-test.sh
@@ -9,8 +9,10 @@ function logAndRun() {
     eval "${@}"
 }
 
-find "${DIR}/local" | xargs touch -d '1978-08-03 01:23:45.6789987Z'
 logAndRun "${GDRSYNC}" -vv -dr "${DIR}/remote" /test
+
+find "${DIR}/local" | xargs touch -d '1978-08-03 01:23:45.6789987Z'
+touch -d '1970-01-01 00:00:00Z' "${DIR}/local/invalidDate"
 
 cat <<-EOF
 Rename using the web interface:
