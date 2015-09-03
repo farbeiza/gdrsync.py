89300b9e4e6ce64bc2249273ad52bcb89f5c62ba
Fernando Arbeiza <fernando.arbeiza@gmail.com>
2015-09-04 01:15:46 +0200

    Fix the rounding of dates.

--- date.py
+++ date.py
@@ -3,55 +3,64 @@
 import calendar
 import datetime
 import functools
+import math
 
 MS = 1000 # milliseconds / second
 US = MS * MS # microseconds / second
 
-# Time resolution in Google Drive is milliseconds
-SCALE = 3
-PRECISION = 10 ** SCALE
+DATE_TIME_PARSE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
-DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+DATE_TIME_PRINT_FORMAT = '%Y-%m-%dT%H:%M:%S'
+MILLIS_OF_SECOND_PRINT_FORMAT = '.%sZ'
 
-# INVALID_SECONDS is a special value used in Google Drive for empty Dates
-INVALID_SECONDS = 0.0
-SUBSTITUTE_SECONDS = float(1) / PRECISION
+# INVALID_MILLIS is a special value used in Google Drive for empty Dates
+INVALID_MILLIS = 0
+SUBSTITUTE_SECONDS = float(1) / MS
 
 def fromSeconds(seconds):
-    return Date(seconds)
+    return Date(seconds * MS)
 
 def fromString(string):
-    dateTime = datetime.datetime.strptime(string, DATE_TIME_FORMAT)
+    dateTime = datetime.datetime.strptime(string, DATE_TIME_PARSE_FORMAT)
     seconds = (calendar.timegm(dateTime.timetuple())
             + (float(dateTime.microsecond) / US))
     if seconds == SUBSTITUTE_SECONDS:
-        seconds = INVALID_SECONDS
+        return invalidDate()
 
     return fromSeconds(seconds)
 
+def invalidDate():
+    return Date(INVALID_MILLIS)
+
 @functools.total_ordering
 class Date(object):
-    def __init__(self, seconds):
-        self._seconds = round(seconds, SCALE)
+    def __init__(self, millis):
+        # Time resolution in Google Drive is milliseconds
+        self._millis = round(millis)
 
     @property
     def seconds(self):
-        return self._seconds
+        return float(self._millis) / MS
 
     def __eq__(self, other):
-        return self._seconds == other._seconds
+        return self._millis == other._millis
 
     def __lt__(self, other):
-        return self._seconds < other._seconds
+        return self._millis < other._millis
 
     def __hash__(self):
-        return round(self._seconds * PRECISION)
+        return self._millis
 
     def __str__(self):
-        seconds = self._seconds
-        if seconds == INVALID_SECONDS:
+        seconds = self.seconds
+        if self._millis == INVALID_MILLIS:
             seconds = SUBSTITUTE_SECONDS
 
         dateTime = datetime.datetime.utcfromtimestamp(seconds)
 
-        return dateTime.strftime(DATE_TIME_FORMAT)
+        # strftime does not have a format directive for milliseconds
+        millisOfSecond, _ = math.modf(seconds)
+        millisOfSecond = round(millisOfSecond * MS)
+
+        return (dateTime.strftime(DATE_TIME_PRINT_FORMAT)
+                + (MILLIS_OF_SECOND_PRINT_FORMAT % millisOfSecond))
